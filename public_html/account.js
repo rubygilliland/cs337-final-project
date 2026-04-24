b4b256bfa2a303607171bd2ec7311e12d6d56bbd
Dalia2a <dalia2alshammari@arizona.edu>
2026-04-24 23:24:34 +0300

    added login and register pages with account.js but not finished yet

---
+++ public_html/account.js
@@ -0,0 +1,56 @@
+
+//login
+const loginForm = document.getElementById("loginForm");
+if (loginForm) {
+  loginForm.addEventListener("submit", async function(e) {
+    e.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const response = await fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ username: username, password: password })
+    });
+    const result = await response.json();
+
+    if (result.message) {
+      document.getElementById("message").textContent = result.message;
+    }
+
+    if (result.success) {
+      window.location.href = "home.html";
+    }
+  });
+}
+
+//register
+const registerForm = document.getElementById("registerForm");
+if (registerForm) {
+  registerForm.addEventListener("submit", async function(e) {
+    e.preventDefault();
+
+    const username = document.getElementById("newUsername").value;
+    const password = document.getElementById("newPassword").value;
+
+    const response = await fetch("/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ username: username, password: password })
+    });
+
+    const result = await response.json();
+
+    if (result.message) {
+      document.getElementById("message").textContent = result.message;
+    }
+
+    if (result.success) {
+      window.location.href = "login.html";
+    }
+  });
+}
